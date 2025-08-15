8e5dd7f37a27e91f2ec1d4dada488ad8606922c4
razorblade23 <81590614+razorblade23@users.noreply.github.com>
2025-08-15 15:07:21 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ async function run(): Promise<void> {
     const versionInput = core.getInput('version')
 
     const platformMap: Record<string, string> = {
-            win32: 'x86_64-pc-windows-msvc.exe',
-            linux: 'x86_64-unknown-linux-gnu',
-            darwin: 'x86_64-apple-darwin',
+            win32: 'pycrucible_v0.3.0-pypi-fix3_x86_64-pc-windows-msvc.exe',
+            linux: 'pycrucible_v0.3.0-pypi-fix3_x86_64-unknown-linux-gnu',
+            darwin: 'pycrucible_v0.3.0-pypi-fix3_x86_64-apple-darwin',
         };
 
     const platform = os.platform()
@@ -26,7 +26,7 @@ async function run(): Promise<void> {
     if (!asset) throw new Error(`Unsupported platform: ${platform}`)
 
     const version = versionInput || await getLatestRelease()
-    const binUrl = `https://github.com/razorblade23/PyCrucible/releases/download/v0.3.0-pypi-fix3/pycrucible_v0.3.0-pypi-fix3_${asset}`
+    const binUrl = `https://github.com/razorblade23/PyCrucible/releases/download/v0.3.0-pypi-fix3/${asset}`
     const binDir = path.join(process.cwd(), 'pycrucible_bin')
     const binPath = path.join(binDir, platform === 'win32' ? 'pycrucible.exe' : 'pycrucible')
 
