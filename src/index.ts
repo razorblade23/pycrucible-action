536f05aaff4f43c62e9a9349fa324a1f1f7a6597
Jeel Dobariya <136002730+JeelDobariya38@users.noreply.github.com>
2025-11-25 19:44:53 +0530

    Fix: Latest version flag usage bug, introduced in v3. (#1)
    * fix: the version name typo
    
    * ci: update auto commit worflow
    
    * Apply automatic changes
    
    * chore: fix license typo
    
    * ci: improve readablity of the workflow
    
    * Apply automatic changes
    
    * chore: remove unwanted files
    
    * chore: add a github action badge in README
    
    * ci: improve autocommit message
    
    * feat: add default value for version in action.yml

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ async function run(): Promise<void> {
   try {
     const entry = core.getInput('entry') || '.'
     const output = core.getInput('output') || 'dist/app'
-    const versionInput = core.getInput('version') || 'pycrucible'
+    const versionInput = core.getInput('version') || 'latest'
 
     // Install PyCrucible (optionally pinned)
     if (versionInput === 'latest') {
